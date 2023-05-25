a282378a4aa217760dd9a8417b3b2211f544bc24
Daniel Adu-Gyan <danny_adu@hotmail.com>
2023-05-25 22:21:25 +0200

    Clean up code

--- js/index.js
+++ js/index.js
@@ -1,25 +1,23 @@
-import {scenery} from './sceneryHandler.js'
-import * as renderHandler from './renderHandler.js'
-import * as loader from './loader.js'
+import { scenery } from "./sceneryHandler.js";
+import * as renderHandler from "./renderHandler.js";
+import * as loader from "./loader.js";
 
-const scene = scenery.scene
+const scene = scenery.scene;
 
+loader.loadGround("Models/ELEKARTA.glb", 0.14, -12, -16, 8, -Math.PI / 2);
+loader.loadFloor("Models/Våning0.glb", 0.1, 0, -20, 0);
 
-loader.loadGround("Models/ELEKARTA.glb", 0.14, -12, -16, 8, -Math.PI/2)
-//load("Models/ELEKARTA.glb", 0.14, -12, -16, 8, -Math.PI/2)
-loader.loadFloor("Models/Våning0.glb", 0.1, 0, -20, 0)
+scene.add(scenery.skybox);
+scene.add(scenery.sphere);
+scene.add(scenery.arrow);
 
-scene.add(scenery.skybox)
-scene.add(scenery.sphere)
-scene.add(scenery.arrow)
+scene.add(renderHandler.camera);
+scene.add(renderHandler.lights.light1);
+scene.add(renderHandler.lights.light2);
 
-scene.add(renderHandler.camera)
-scene.add(renderHandler.lights.light1)
-scene.add(renderHandler.lights.light2)
-
-animate()
-
-function animate(){
-    requestAnimationFrame(animate)
-    renderHandler.renderer.render(scene, renderHandler.camera)
+function animate() {
+  requestAnimationFrame(animate);
+  renderHandler.renderer.render(scene, renderHandler.camera);
 }
+
+animate();
